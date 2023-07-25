5be5097482d2685f538669e3403ffd8b67420145
Julli4n <11952329+Julli4n@users.noreply.github.com>
2023-07-24 20:03:23 -0700

    update deps

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,6 +1,6 @@
-export { parse as parseJSONC } from "https://deno.land/std@0.193.0/jsonc/mod.ts";
-export { type MessageFormatElement } from "https://esm.sh/v128/@formatjs/icu-messageformat-parser@2.4.0";
-export { default as I18n } from "https://esm.sh/v128/intl-messageformat@10.3.5";
+export { parse as parseJSONC } from "https://deno.land/std@0.195.0/jsonc/mod.ts";
+export { type MessageFormatElement } from "https://esm.sh/v129/@formatjs/icu-messageformat-parser@2.6.0";
+export { default as I18n } from "https://esm.sh/v129/intl-messageformat@10.4.0";
 
 export type MessagesFile = Record<string, {
     message: string;
